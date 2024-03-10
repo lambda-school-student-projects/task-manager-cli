c656dfa7d546edf39a78044cc33001e2f451dd19
Joshua Kaunert <44586402+jkaunert@users.noreply.github.com>
2024-03-10 23:11:21 +0000

    implements `TaskManager.add_task(input)`

--- task_manager.rb
+++ task_manager.rb
@@ -1,13 +1,25 @@
+require_relative 'task'
 
 class TaskManager
-  attr_accessor :tasks
+  attr_reader :tasks
 
   def initialize
     @tasks = []
   end
 
+  def add_task(input)
+    puts "Enter task name:"
+    task_name = input.gets&.chomp
+    puts "Enter due date:"
+    task_due_date = input.gets&.chomp
+    task_status = "Incomplete"
+
+    new_task = Task.new(task_name, task_due_date, task_status)
+    @tasks << new_task
+    puts "Task added!"
+  end
+
   def run
   
   end
-
 end
--- tests/test_task_manager.rb
+++ tests/test_task_manager.rb
@@ -12,5 +12,28 @@ class TestTaskManager < MiniTest::Test
     assert_equal [], @task_manager.tasks
   end
 
+  def test_add_task
+  input = StringIO.new("Buy cat food\n03-13-2024\n")
+  @task_manager.add_task(input)
+  assert_equal 1, @task_manager.tasks.length
+  
+  task = @task_manager.tasks.first
+  assert_equal 'Buy cat food', task.name
+  assert_equal '03-13-2024', task.due_date
+  assert_equal 'Incomplete', task.status
+
+  end
+
+  def test_update_task
+  end
+
+  def test_remove_task
+  end
+
+  def test_display_tasks
+  end
+
+  def test_run
+  end
 
 end
