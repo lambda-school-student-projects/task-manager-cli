e04ae85a18bb8216b61fa6f39a9e68ef9a493ffc
Joshua Kaunert <44586402+jkaunert@users.noreply.github.com>
2024-03-10 22:07:44 +0000

    updates tests for DRY

--- tests/test_task.rb
+++ tests/test_task.rb
@@ -3,37 +3,34 @@ require_relative '../task'
 
 class TestTask < MiniTest::Test
   def setup
+    @task = Task.new("Buy clothing", "05-05-2000", "Incomplete")
   end
 
-  def test_create_task
-    task = Task.new("Buy clothing", "05-05-2000", "Incomplete")
-    assert_equal "Buy clothing", task.name
-    assert_equal "05-05-2000", task.due_date
-    assert_equal "Incomplete", task.status
+  def test_initialize
+    assert_equal "Buy clothing", @task.name
+    assert_equal "05-05-2000", @task.due_date
+    assert_equal "Incomplete", @task.status
   end
 
-  def test_update_task_name
-    task = Task.new("Buy clothing", "05-05-2000", "Incomplete")
+  def test_update_accessor_name
     new_task_name = "Clean the litter box"
-    task.name = new_task_name
+    @task.name = new_task_name
 
-    assert_equal new_task_name, task.name
+    assert_equal new_task_name, @task.name
   end
 
-  def test_update_task_due_date
-    task = Task.new("Buy clothing", "05-05-2000", "Incomplete")
+  def test_update_accessor_due_date
     new_task_due_date = "10-31-2024"
-    task.due_date = new_task_due_date
+    @task.due_date = new_task_due_date
 
-    assert_equal new_task_due_date, task.due_date
+    assert_equal new_task_due_date, @task.due_date
   end
 
-  def test_update_task_status
-    task = Task.new("Buy clothing", "05-05-2000", "Incomplete")
+  def test_update_accessor_status
     new_task_status = "Completed"
-    task.status = new_task_status
+    @task.status = new_task_status
 
-    assert_equal new_task_status, task.status
+    assert_equal new_task_status, @task.status
   end
 
 end
