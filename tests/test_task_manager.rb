96ae16f9c05c9a4393a46c36fa2677b5f4c36e4c
Joshua Kaunert <44586402+jkaunert@users.noreply.github.com>
2024-03-11 02:10:23 +0000

    implements `TaskManager.run(input)`

--- tests/test_task_manager.rb
+++ tests/test_task_manager.rb
@@ -13,15 +13,14 @@ class TestTaskManager < MiniTest::Test
   end
 
   def test_add_task
-  input = StringIO.new("Buy cat food\n03-13-2024\n")
-  @task_manager.add_task(input)
-  assert_equal 1, @task_manager.tasks.length
-  
-  task = @task_manager.tasks.first
-  assert_equal 'Buy cat food', task.name
-  assert_equal '03-13-2024', task.due_date
-  assert_equal 'Incomplete', task.status
-
+    input = StringIO.new("Buy cat food\n03-13-2024\n")
+    @task_manager.add_task(input)
+    assert_equal 1, @task_manager.tasks.length
+    
+    task = @task_manager.tasks.first
+    assert_equal 'Buy cat food', task.name
+    assert_equal '03-13-2024', task.due_date
+    assert_equal 'Incomplete', task.status
   end
 
   def test_update_task
@@ -69,6 +68,30 @@ class TestTaskManager < MiniTest::Test
   end
 
   def test_run
+    input = StringIO.new("add\nBuy cat food\n03-13-2024\ndisplay\nexit\n")
+    output = StringIO.new
+    $stdout = output
+
+    @task_manager.run(input)
+
+    expected_output = <<~OUTPUT
+      What would you like to do? (add/update/remove/display/exit)
+      Enter task name:
+      Enter due date:
+      Task added!
+      What would you like to do? (add/update/remove/display/exit)
+      Tasks:
+      Task Name: Buy cat food
+      Due Date: 03-13-2024
+      Status: Incomplete
+      ------------------------
+      What would you like to do? (add/update/remove/display/exit)
+      Thank you for using the TaskMaster app!
+    OUTPUT
+
+    assert_equal expected_output, output.string
+  ensure
+    $stdout = STDOUT
   end
 
 end
